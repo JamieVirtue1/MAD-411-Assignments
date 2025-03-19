75925d80d83a95ecc6083cf8702a64b65edb79bd
Jamie Virtue <w0852328@myscc.ca>
2025-03-19 17:15:12 -0400

    Added intents and passed values between activities

---
+++ Android8/app/src/main/java/com/example/android6/ExpenseDetailsActivity.kt
@@ -0,0 +1,33 @@
+package com.example.android8
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ExpenseDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_detail)
+
+        //pulling data from main activity
+        val expenseName = intent.getStringExtra("NAME_OF_EXPENSE")
+        //required default value
+        val expenseAmount = intent.getDoubleExtra("AMOUNT_OF_EXPENSE", 0.0)
+
+        val expenseDate = intent.getStringExtra("DATE_OF_EXPENSE")
+
+        //binding the retrieved data to my text views
+        val nameTextView = findViewById<TextView>(R.id.expenseNameDetails)
+        val amountTextView = findViewById<TextView>(R.id.expenseAmountDetails)
+        val dateTextView = findViewById<TextView>(R.id.expenseDateDetails)
+
+        //formatting my text to display on ExpenseDetailsActivity view
+        nameTextView.text = "Expense Name: $expenseName"
+
+        val formattedAmount = String.format("$%.2f", expenseAmount)
+        amountTextView.text = "Cost of Expense: $formattedAmount"
+
+        dateTextView.text = "Date: $expenseDate"
+    }
+}
--- Android8/app/src/main/java/com/example/android6/MainActivity.kt
+++ Android8/app/src/main/java/com/example/android6/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.example.android7
+package com.example.android8
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Layout
 import android.util.Log
@@ -9,18 +11,22 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.sql.Date
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var expenseName: EditText
     lateinit var expenseAmount: EditText
+    lateinit var expenseDate: DatePicker
     lateinit var expenseRecyclerView: RecyclerView
     lateinit var addExpenseButton: Button
+    lateinit var financialTipsButton: Button
     lateinit var recyclerViewAdapter: ExpenseAdapter
-    val listOfExpenses = mutableListOf<Pair<String, Double>>()
+    val listOfExpenses = mutableListOf<Pair<String, Pair<String, Double>>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +38,15 @@ class MainActivity : AppCompatActivity() {
 
         expenseName = findViewById(R.id.expenseName)
         expenseAmount = findViewById(R.id.expenseAmount)
+        expenseDate = findViewById(R.id.expenseDate)
 
         //recycler view initialization and linear layout application
         expenseRecyclerView = findViewById(R.id.expenseRecyclerView)
         expenseRecyclerView.layoutManager = LinearLayoutManager(this)
 
         addExpenseButton = findViewById(R.id.addExpenseButton)
+
+        financialTipsButton = findViewById(R.id.financialTipsButton)
 
 
         //setting the adapter to the recycler view (not sure why i cant simplify this)
@@ -49,11 +58,17 @@ class MainActivity : AppCompatActivity() {
             val name = expenseName.text.toString()
             val cost = expenseAmount.text.toString()
 
+            val day = expenseDate.dayOfMonth
+            val month = expenseDate.month + 1
+            val year = expenseDate.year
+
+            val date = "$day/$month/$year"
+
             //checking to make sure variables arent empty
             if (name.isNotEmpty() && cost.isNotEmpty()) {
 
-                //adding expense to list that takes pair of string and double
-                val expense = Pair(name, cost.toDouble())
+                //adding expense to list that takes pair of string and double and a solo string
+                val expense = Pair(name, Pair(date, cost.toDouble()))
                 listOfExpenses.add(expense)
                 //notifying the adapter a change has been made at this specific index
                 recyclerViewAdapter.notifyItemInserted(listOfExpenses.size - 1)
@@ -63,6 +78,16 @@ class MainActivity : AppCompatActivity() {
                 expenseAmount.text.clear()
 
             }
+        }
+
+        //button that sends to financial help
+        financialTipsButton.setOnClickListener {
+            val url = "https://www.cibc.com/en/business/advice-centre/articles/financial-tips.html"
+
+            val financialTipsIntent = Intent(Intent.ACTION_VIEW)
+            financialTipsIntent.data = Uri.parse(url)
+
+            startActivity(financialTipsIntent)
         }
     }
 
@@ -105,7 +130,7 @@ class MainActivity : AppCompatActivity() {
 
 //adapter class for the RecyclerView
 class ExpenseAdapter(
-    private val expenses: MutableList<Pair<String, Double>>,
+    private val expenses: MutableList<Pair<String, Pair<String, Double>>>,
 ) : RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder>() {
 
     //onCreateViewHolder function that runs when view created
@@ -143,6 +168,16 @@ class ExpenseAdapter(
             textSize = 16f
         }
 
+        //textview for the date field
+        val dateTextView = TextView(parent.context).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(0,20,0,20)
+            }
+        }
+
         //button for deleting the expense from the recycler view
         val deleteButton = Button(parent.context).apply {
             text = "Delete"
@@ -151,32 +186,64 @@ class ExpenseAdapter(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
-
         }
+
+        //button for showing item details
+        val showDetailsButton = Button(parent.context).apply{
+            text = "Show Details"
+
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
+
 
         //adding the recyclerview fields to the layout
         layout.addView(nameTextView)
         layout.addView(amountTextView)
         layout.addView(deleteButton)
+        layout.addView(showDetailsButton)
 
         //returning the recyclerview with the created layout
-        return ExpenseViewHolder(layout, nameTextView, amountTextView, deleteButton)
+        return ExpenseViewHolder(layout, nameTextView, amountTextView, dateTextView, deleteButton, showDetailsButton)
     }
 
     //binding my data to my view holder
     override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
         //setting individual expenses based on their position in the expenses array
         val expense = expenses[position]
+
+        val name = expense.first
+        val date = expense.second.first
+        val amount = expense.second.second
+
         //setting my first pair value to name
-        holder.nameTextView.text = expense.first
+        holder.nameTextView.text = name
         //setting my second pair value to cost amount
-        holder.amountTextView.text = "$${expense.second}"
+        holder.amountTextView.text = "$$amount"
+
+        holder.dateTextView.text = date
+
 
         //set the delete button listener
         holder.deleteButton.setOnClickListener {
 
             expenses.removeAt(position)
             notifyItemRemoved(position)
+        }
+
+        //button that swaps screen to ExpenseDetailsActivity
+        holder.showDetailsButton.setOnClickListener{
+
+            val passedInformation = holder.itemView.context
+            val sendDataIntent = Intent(passedInformation, ExpenseDetailsActivity::class.java).apply {
+                //passing data using intent extras
+                putExtra("NAME_OF_EXPENSE", expense.first)
+                putExtra("AMOUNT_OF_EXPENSE", expense.second.second)
+                putExtra("DATE_OF_EXPENSE", expense.second.first)
+            }
+            passedInformation.startActivity(sendDataIntent)
         }
     }
 
@@ -188,6 +255,8 @@ class ExpenseAdapter(
         itemView: View,
         val nameTextView: TextView,
         val amountTextView: TextView,
-        val deleteButton: Button
+        val dateTextView: TextView,
+        val deleteButton: Button,
+        val showDetailsButton: Button
     ) : RecyclerView.ViewHolder(itemView)
 }
